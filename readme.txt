bf2e80a4ffc2b08d622105a0aa28c2f6152fb185
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-11-15 14:56:21 -0800

    Fixed the bug that prevented a launch.
    "launchctl load" was talking to my user login session's launchd, not
    with the system's launchd. It looks like wrappin stuff up into sudo
    solves this problem.

---
+++ readme.txt
@@ -0,0 +1,2 @@
+http://developer.apple.com/library/mac/#technotes/tn2083/_index.html
+  Daemons and Agents
--- src/main/resources/org/jenkinsci/modules/launchd_slave_installer/install.sh
+++ src/main/resources/org/jenkinsci/modules/launchd_slave_installer/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -ex
-JAR=/System/Library/LaunchDaemons/org.jenkins-ci.slave.$3.jar
-DST=/System/Library/LaunchDaemons/org.jenkins-ci.slave.$3.plist
-cp "$1" $DST
-cp "$2" $JAR
-chmod 644 $DST
-launchctl load $DST
+(
+    JAR=/System/Library/LaunchDaemons/org.jenkins-ci.slave.$3.jar
+    DST=/System/Library/LaunchDaemons/org.jenkins-ci.slave.$3.plist
+    cp "$1" $DST
+    cp "$2" $JAR
+    chmod 644 $DST
+    # su failed for no apparent reasons
+    # su - root -c "launchctl load $DST"
+    # this seemingly pointless sudo is necessary to let launchctl connect to the system-level launchd instance
+    sudo -n -u root launchctl load $DST
+    echo success
+) 2>&1 | tee /tmp/jenkins-slave-launchd-install.log
